eb8408a4324f2e415f12762468a01f4c5002af0c
Aarushi Buddhavarapu <baarush@uw.edu>
2024-05-18 16:05:44 -0700

    treatment and quiz forms

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import {SummaryApp} from './summary';
+import { IntroQuiz } from './quiz';
+import { TreatmentQuiz } from './treatment';
+
 
 const App = () => {
   return (
     <div>
+      <IntroQuiz></IntroQuiz>
+      <TreatmentQuiz></TreatmentQuiz>
         <SummaryApp></SummaryApp>
     </div>
   )
