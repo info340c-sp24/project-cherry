831a695b63e47e5f0aa363bb98110398bd2bc7b5
avabui19 <ava19@uw.edu>
2024-05-19 13:46:55 -0700

    summary footer

--- src/summary.js
+++ src/summary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 //import './css/summary.css';
 import './css/styles.css';
+import {Footer} from './footer';
 
 function Navbar() {
   return (
@@ -35,23 +36,6 @@ function SummaryCard({ title, text, imgSrc, alt }) {
         </div>
       </div>
     </div>
-  );
-}
-
-function Footer() {
-  return (
-    <footer>
-      <p>&copy; 2024 Cherry Company. All rights reserved.</p>
-      <ul>
-        <li>Phone: +1234567890</li>
-        <li>Email: <a href="mailto:support@cherrytracker.com">support@cherrytracker.com</a></li>
-        <li>Socials:
-          <a href="http://twitter.com/" target="_blank" title="Follow us on Twitter">Twitter</a>,
-          <a href="http://facebook.com/" target="_blank" title="Like us on Facebook">Facebook</a>,
-          <a href="http://instagram.com/" target="_blank" title="Follow us on Instagram">Instagram</a>
-        </li>
-      </ul>
-    </footer>
   );
 }
 
